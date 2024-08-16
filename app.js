323b086fc4c551ad7dc2049d3b3bdcce890526b4
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-08-16 22:41:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const patientRoutes = require("./routes/patientRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const patientHealthMetricsRoutes = require("./routes/patientHealthMetricsRoutes");
+const { verifyToken } = require("./jwt-middleware");
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:5173" }));
+
+// MongoDB Connection
+mongoose.connect(MONGO_URI);
+mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
+mongoose.connection.on("error", (err) =>
+  console.log("Error connecting to MongoDB:", err)
+);
+
+app.use("/api/doctors", doctorRoutes);
+app.use(verifyToken);
+app.use("/api/patients", patientRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/patient-health-metrics", patientHealthMetricsRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ jwt-middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+ const authHeader = req.headers['authorization'];
+ const token = authHeader && authHeader.split(' ')[1]; 
+
+
+ if (token == null) {
+   return res.status(401).json({ message: 'No token provided' });
+ }
+
+
+ jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+   if (err) {
+     return res.status(403).json({ message: 'Invalid token' });
+   }
+
+
+   req.user = user; 
+   next(); 
+ });
+};
+
+module.exports = {verifyToken }

09eef7173c8ffbb5635ad4ee8fb884c446e82605
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-08-18 12:52:06 +0530

    UPDATE

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
 mongoose.connection.on("error", (err) =>
   console.log("Error connecting to MongoDB:", err)
 );
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Healthcare API!");
+});
 app.get("/ping", (_, res) => {
   res.send("pong");
 });
